82d269e98fad9f0dda4e83c65884815f423e19cf
Galindo <matheusgalindo1313@gmail.com>
2022-09-23 22:09:24 -0300

    escolher palavra completo e habilitar botão incompleto, erro ao selecionar o segundo botão

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import palavras from './palavras'
-import alfabeto from './alfabeto'
+import palavras from "./palavras";
+import alfabeto from "./alfabeto";
+import React, { useState } from "react";
 
 export default function App() {
+  const sorteio =  palavras.sort(() => Math.random() - 0.5);
+  const [wordDrawn, setWordDrawn] = React.useState(sorteio[0]);
+  const [wordLetters, setWordLetters] = React.useState("");
+  const [estadoAlfabeto, setEstadoAlfabeto] = React.useState(alfabeto.map((letra) => letra = true))
+  const [mistakes, setMistakes] = React.useState(0);
 
+  function chooseWord() {
+    setWordDrawn(sorteio[1]);
+    setWordLetters(wordDrawn.split('').map((l) => l = '_ '))
+  }
 
+  function verifyButton(i) {
+    if (estadoAlfabeto[i] !== false) {
+      setEstadoAlfabeto(...estadoAlfabeto, estadoAlfabeto[i] = false)
+    }
+
+    console.log(estadoAlfabeto);
+  }
 
   return (
     <div className="game">
@@ -12,12 +29,20 @@ export default function App() {
           <img src="assets/forca0.png" alt="hangman0" />
         </div>
         <div className="hangmanButtonLetters">
-          <button>Escolher Palavra</button>
-          <div className="buttonLetters"></div>
+          <button onClick={chooseWord}>Escolher Palavra</button>
+          <div className="buttonLetters">
+            <p>{wordLetters}</p>
+          </div>
         </div>
       </div>
       <div className="keyboard">
-        {alfabeto.map ((letter, index) => <button className='letter'>{letter}</button>)}
+        {alfabeto.map((letter, index) => (
+          <button key={index}
+            onClick={() => verifyButton(index)}
+            className="letter">
+            {letter}
+          </button>
+        ))}
       </div>
       <div className="toGuess">
         <p>Já sei a palavra!</p>
